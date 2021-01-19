4f4eb1e8557150df8bc12e057871eac1b5fa4567
Dhaval Soneji <dhaval@soneji.xyz>
2021-01-19 17:24:46 +0000

    Initial commit

---
+++ ES2C4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ES2C4
+//
+//  Created by Dhaval Soneji on 19/01/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
